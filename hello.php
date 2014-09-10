b117bbf29eb285abd6401d43246657b7345bcfad
Gd46 <gd46@njit.edu>
2014-09-10 11:15:36 -0400

    Added Object Oriented Test

--- hello.php
+++ hello.php
@@ -2,9 +2,26 @@
 
 	// Single quotes are for literals.
 	// Double quotes allow to mix variables in string.
-	
+	// Imperative Way 
+
 	/* variable */ $class = 'is218';
 	
 	echo 'Hello World' .  $class;
+
+	// Object Oriented Way
+
+	$obj = new example;
+	class example {
+		
+		// Object is initiated, constructor - functions
+		// Functions are inside of a class
+		// Methods are what you call to active functions
+		
+		//Constructor
+
+		function __construct() {
+			echo 'Test';
+		}
+	}
 	
 ?>
