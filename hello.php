b927a17c629915df51de43b34db549a4da6e3d0e
Gd46 <gd46@njit.edu>
2014-09-10 12:37:46 -0400

    Added notes and more on OOP

--- hello.php
+++ hello.php
@@ -6,20 +6,26 @@
 
 	/* variable */ $class = 'is218';
 	
-	echo 'Hello World' .  $class;
+	echo 'Hello  ' .  $class . "<br>";
 
 	// Object Oriented Way
 	
+	// This is how you print the contents of an array or object
+	print_r($obj);
+
+	// This is how you instantiate an object
+	$obj = new example;
+
+	// This is how you assign a value to a public property
 	$obj->name = 'steve';
 
-	//print_r($obj);
-
-	$obj = new example;
-	$obj2 = new example('giuseppe');
+	// This is how you call a public method
 	$obj->printHelloWorld();
 
+	// This is how you define a class
 	class example {
-
+		
+		// This is how you define a public property
 		public $name;
 		
 		// Object is initiated, constructor - functions
@@ -28,13 +34,16 @@
 		
 		//Constructor - automatically called when object is initiated 
 
-		function __construct() {		
+		public function __construct() {		
 			echo 'Hi ' . $this->name;
+			echo "<br>";
 		}
 
-		function printHelloWorld(){
+		public function printHelloWorld(){
 			echo 'Hi ' . $this->name;
+			echo "<br>";
 		}
+		
 	}
 	
 ?>
